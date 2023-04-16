a0c5437db1d10e73e1076e2d42d421405a9574a5
dimaD2003 <113402321+dimaD2003@users.noreply.github.com>
2023-04-16 19:06:08 +1000

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} 
+  @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+Display:ital,wght@1,600&family=Rock+Salt&display=swap');*/
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    letterSpacing: {
+      '1': '-0.26em',},
+    fontFamily: {
+      display: ['Oswald'],},
     container:{center:true},
     extend: {}
   },
