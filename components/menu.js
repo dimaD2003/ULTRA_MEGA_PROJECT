32b8e4b0220d3d71ae5d93ac549c35ad729aedad
pozzy21 <pozzy199911@gmail.com>
2023-04-16 14:16:06 +1000

    fix links

--- components/menu.js
+++ components/menu.js
@@ -1,18 +1,18 @@
 import { ConnectWallet } from "@thirdweb-dev/react";
-
+import Link from 'next/link'
 export default function HeadMenu() {
 
 
     return (
         <div id = "container" className = "container mx-auto">
-            <a href="../"><div class="bg-white border-4 text-center rounded-lg mt-5 ">Home</div></a>
+            <Link  href="../"><div class="bg-white border-4 text-center rounded-lg mt-5 ">Home</div></Link>
             <div class = "grid grid-cols-7 gap-2 mt-12 bg-gray-400 bg-opacity-30 text-2xl text-black">
-            <a href="/swappage"><div class="bg-white border-4 text-center rounded-lg">Swap</div></a>
-            <a href="/mint"><div class="bg-white border-4 text-center rounded-lg">Mint</div></a>
-            <a href="/stake"><div class="bg-white border-4 text-center rounded-lg">Stake</div></a>
-            <a href="/vote"><div class="bg-white border-4 text-center rounded-lg">Vote</div></a>
-            <a href="/marketplace"><div class="bg-white border-4 text-center rounded-lg ">Marketplace</div></a>
-            <a href="/contact"><div class="bg-white border-4 text-center rounded-lg">Contact</div></a>
+            <Link href="/swappage"><div class="bg-white border-4 text-center rounded-lg">Swap</div></Link>
+            <Link href="/mint"><div class="bg-white border-4 text-center rounded-lg">Mint</div></Link>
+            <Link href="/stake"><div class="bg-white border-4 text-center rounded-lg">Stake</div></Link>
+            <Link href="/vote"><div class="bg-white border-4 text-center rounded-lg">Vote</div></Link>
+            <Link href="/marketplace"><div class="bg-white border-4 text-center rounded-lg ">Marketplace</div></Link>
+            <Link href="/contact"><div class="bg-white border-4 text-center rounded-lg">Contact</div></Link>
             <ConnectWallet/>
             </div>
         </div>
